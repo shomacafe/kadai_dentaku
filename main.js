4e3e990be320af4379a586ba1c114a94bbc44960
EC2 Default User <ec2-user@ip-172-31-94-44.ec2.internal>
2022-02-18 06:13:16 +0000

    dentaku kadai

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 let result = document.getElementById("result");
+let operator = ["+","-","*","/"];//変数の宣言
 
 function edit(elem){
+ //一番右の文字列が四則演算子かつ入力された文字列が四則演算子のとき
+  if(operator.includes(result.value.slice(-1)) == true && operator.includes(elem.value) == true){
+  console.log("四則演算子が置き換わりました。")
+  let f = result.value.slice(0,-1);
+  result.value = f + elem.value;
+}else{
+  
   if(result.value == 0){
     result.value = elem.value;
   }else{
     result.value += elem.value;
   }
 }
-
-let operator = document.getElementsByClassName("operator");
-
-function ope(operator){
-  if(result.value.slice(-1) === operator.value ){
-    const f = result.value.slice(0,-1)
-    result.value = f + operator.value;
-  }else{
-    result.value += operator.value;
-  }
 }
 
 function calc() {
